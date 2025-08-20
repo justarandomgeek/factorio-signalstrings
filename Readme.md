fcca13ac1a4379d2c0f7eb68395c1be1f2928d45
justarandomgeek <justarandomgeek@gmail.com>
2025-08-20 17:46:42 -0400

    update signalstrings for 2.0 using mod-data

--- Readme.md
+++ Readme.md
@@ -1,10 +1,33 @@
 ## SignalStrings
 
 This library provides common functions for handling strings on circuit networks. Strings are expressed as a bitmask of where each letter appears, LSB on the left.
-"FOOBAR" = {signal-F=1,signal-O=6,signal-B=8,signal-A=16,signal-R=32}
+"foobar" = {signal-F=1,signal-O=6,signal-B=8,signal-A=16,signal-R=32}
 
-Strings may be converted to signals using `remote.call('signalstrings','string_to_signals',"FOOBAR")`, and optionally with extra data (in the form of Constant Combinator params to be prefixed) using `remote.call('signalstrings','string_to_signals',"FOOBAR", extrasignals)`. Strings are automatically converted to all-caps to ease conversion, and non-representable characters are discarded (leaving empty space). Item and fluid rich text tags are converted to the matching signals.
+Signals without mappings will fall back to a richtext tag if possible. If quality is enabled, uncommon signals will be used for capital letters. Mods may register additional signal mappings by adding them to `data.raw["mod-data"]["signalstrings-mapping"].data` (`{[string]:SignalID}`), using a single utf8 encoded character as the key.
 
-Signals may be converted back to a string using `remote.call('signalstrings','signals_to_string',signallist, userichtags)`. If `userichtags` is `true`, item and fluid signals will be converted to their richtext tags.
 
-Mods that add signals may register them for string conversions by calling `remote.call('signalstrings','register_signal','signal-name','X')` in their on_load and on_init events.
+```lua
+local sigstr = require("__signalstrings__/signalstrings.lua")
+
+-- read a string from signals...
+local str = sigstr.signals_to_string(entity.get_signals(defines.wire_connector_id.combinator_input_green))
+
+-- write to a constant combinator...
+control.sections[1].filters = sigstr.string_to_logistic_filters("Hello World!")
+
+-- write to a decider...
+local p = control.parameters
+p.outputs = sigstr.string_to_decider_outputs("Hello World!")
+control.parameters = p
+
+-- also back to signals...
+local sigs = sigstr.string_to_signals("Hello World!")
+
+-- or customize it...
+local Ts = sigstr.string_to_Ts("Hello World!", function (signal, value)
+  return "Earl Grey, Hot"
+end)
+
+```
+
+
---
+++ signalstrings.lua
@@ -0,0 +1,244 @@
+---@alias typeinfo {type:SignalIDType, richtag:string?, richqual:boolean?, protos:LuaCustomTable<string>}
+
+---@type table<SignalIDType, typeinfo>
+local signal_types = {}
+---@type table<string, typeinfo>
+local richtag_types = {}
+
+do
+  ---@type typeinfo[]
+  local typeinfos = {
+    { type = "item", richtag="item", richqual=true, protos = prototypes.item, },
+    { type = "fluid", richtag="fluid", richqual=true, protos = prototypes.fluid, },
+    { type = "virtual", richtag="virtual-signal", richqual=true, protos = prototypes.virtual_signal, },
+    { type = "recipe", richtag="recipe", richqual=true, protos = prototypes.recipe, },
+    { type = "entity", richtag="entity", richqual=true, protos = prototypes.entity, },
+    { type = "space-location", richtag="space-location", protos = prototypes.space_location, },
+    { type = "quality", richtag="quality", protos = prototypes.quality, },
+    { type = "asteroid-chunk", protos = prototypes.asteroid_chunk, },
+  }
+  for _, info in pairs(typeinfos) do
+    signal_types[info.type] = info
+    if info.richtag then
+      richtag_types[info.richtag] = info
+    end
+  end
+end
+
+---@type {[string]:SignalID}
+local c2s=prototypes.mod_data["signalstrings-mapping"].data --[[@as {[string]:SignalID}]]
+
+---@type {[QualityID]:{[SignalIDType]:{[string]:string}}}
+local s2c={}
+--TODO: validate the signal prototypes? or just let them fail later?
+for char, signal in pairs(c2s) do
+  local qmap = s2c[signal.quality or "normal"]
+  if not qmap then
+    qmap = {}
+    s2c[signal.quality or "normal"] = qmap
+  end
+  local tmap = qmap[signal.type or "item"]
+  if not tmap then
+    tmap = {}
+    qmap[signal.type or "item"] = tmap
+  end
+  tmap[signal.name] = char
+end
+
+---@param signal SignalID
+---@return string?
+local function sigchar(signal)
+  if not signal then return end
+  local qmap = s2c[signal.quality or "normal"]
+  if qmap then
+    local tmap = qmap[signal.type or "item"]
+    if tmap then
+      local char = tmap[signal.name]
+      if char then
+        return char
+      end
+    end
+  end
+  local typeinfo = signal_types[signal.type]
+  if typeinfo then
+    if typeinfo.richqual and signal.quality and signal.quality ~= "normal" then
+      -- qual tag
+      return string.format("[%s=%s,quality=%s]", typeinfo.richtag, signal.name, signal.quality)
+    end
+    return string.format("[%s=%s]", typeinfo.richtag, signal.name)
+  end
+end
+
+---@param set Signal[]
+---@return string
+local function signals_to_string(set)
+  local sigbits = {}
+  local bitsleft = -1
+  local lastbit = 0
+  for _,sig in pairs(set) do
+    local newbits = bit32.band(sig.count,bitsleft)
+    if newbits ~= 0 then
+      for i=0,30 do
+        local sigbit = bit32.extract(newbits,i)
+        if sigbit==1 then
+          local ch = sigchar(sig.signal)
+          if ch then
+            sigbits[i+1] = ch
+            bitsleft = bit32.replace(bitsleft,0,i) --[[@as integer]]
+            if lastbit < i then
+              lastbit = i
+            end
+            if bitsleft == 0 then
+              return table.concat(sigbits)
+            end
+          end
+        end
+      end
+    end
+  end
+
+  for i=1,lastbit do
+    if sigbits[i] == nil then
+      sigbits[i]  = " "
+    end
+  end
+
+  return table.concat(sigbits)
+end
+
+---@param str string
+---@param i integer
+---@return SignalID? tag
+---@return string? tagstr
+---@return integer? nexti
+local function try_match_richtext(str,i)
+  local _,j,tagtype,tagname,tagqual
+  if script.feature_flags.quality then
+    _,j,tagtype,tagname,tagqual = str:find("^%[([%a%-]+)=([%a%d%-_]+),quality=([%a%d%-_]+)%]", i)
+  end
+  
+  if not tagtype then
+    tagqual = "normal"
+    _,j,tagtype,tagname = str:find("^%[([%a%-]+)=([%a%d%-_]+)%]", i)
+  end
+
+  if not tagtype then return end
+  ---@cast tagname string
+  ---@cast tagname string
+  ---@cast tagqual string
+  local taginfo = richtag_types[tagtype]
+  if not taginfo.protos[tagname] then return end
+  if not prototypes.quality[tagqual] then return end
+
+  ---@type SignalID
+  local sig = {
+    type = taginfo.type,
+    name = tagname,
+    quality = tagqual,
+  }
+  return sig, str:sub(i,j), j+1
+end
+
+---@generic T
+---@param str string
+---@param signal_format fun(signal:SignalID, value:integer):T
+---@return T[]
+local function string_to_Ts(str,signal_format)
+  local letters = {}
+  local tagsigs = {}
+
+  do
+    local b=1 -- bit position in output
+    local i=1 -- index in str
+    local l=#str
+
+    while i<=l and b < 0x100000000 do
+      local c = str:sub(i,i)
+      local j = i+1
+      if c == "[" then
+        local tagsig,tagstr,tagi = try_match_richtext(str, i)
+        if tagsig then
+          ---@cast tagstr -?
+          ---@cast tagi -?
+          c = tagstr
+          tagsigs[tagstr] = tagsig
+          j = tagi
+        end
+      elseif c:byte() >= 0x80 then
+        local _,uchari, uchar = str:find("^([\xC2-\xF4][\x80-\xBF]+)", i)
+        if uchari then
+          ---@cast uchar -?
+          c = uchar
+          j = uchari+1
+        end
+      end
+      letters[c]=(letters[c] or 0)+b
+      i=j
+      b=b*2
+    end
+  end
+
+  ---@type T[]
+  local signals = {}
+  for c,b in pairs(letters) do
+    if b >= 0x80000000 then b =  b - 0x100000000 end
+    local sig = tagsigs[c] or c2s[c]
+    if sig then
+      signals[#signals+1]=signal_format(sig, b)
+    end
+  end
+
+  return signals
+end
+
+---@param signal SignalID
+---@param value integer
+---@return LogisticFilter
+local function to_logistic_filter(signal, value)
+  return {
+    value = {
+      type = signal.type or "item",
+      name = signal.name,
+      quality = signal.quality or "normal",
+      comparator = "=",
+    },
+    min = value,
+  }
+end
+
+---@param signal SignalID
+---@param value integer
+---@return DeciderCombinatorOutput
+local function to_decider_output(signal, value)
+  return { signal = signal, constant = value, copy_count_from_input=false, }--[[@as DeciderCombinatorOutput]]
+end
+
+---@param signal SignalID
+---@param value integer
+---@return Signal
+local function to_signal(signal, value)
+  return { signal = signal, count = value, }--[[@as Signal]]
+end
+
+return {
+  signals_to_string = signals_to_string,
+  string_to_Ts = string_to_Ts,
+
+  ---@param str string
+  ---@return LogisticFilter[]
+  string_to_logistic_filters = function(str)
+    return string_to_Ts(str, to_logistic_filter)
+  end,
+
+  ---@param str string
+  ---@return DeciderCombinatorOutput[]
+  string_to_decider_outputs = function(str)
+    return string_to_Ts(str, to_decider_output)
+  end,
+
+  ---@param str string
+  ---@return Signal[]
+  string_to_signals = function(str)
+    return string_to_Ts(str, to_signal)
+  end,
+}
